e74355be0b2fad5c90c7f0eabf745524a2b07f21
David Clay <dclay1983@gmail.com>
2019-11-12 21:41:18 -0700

    Change readAll to a promise

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter.js');
+const Promise = require('bluebird');
+const readdir = Promise.promisify(fs.readdir);
 
 var items = {};
 
@@ -16,32 +18,68 @@ exports.create = (text, callback) => {
     });
   });
 };
-
 exports.readAll = (callback) => {
   let todos = [];
   let todo;
-  fs.readdir(exports.dataDir, (err, files) => {
-    if (err) {
-      callback(new Error(`Error accessing ${datDir}`));
-    } else if (files.length) {
-      files.forEach( (file, index) => {
-        id = file.split('.')[0];
-        exports.readOne(id, (err, todo) => {
-          if (err) {
-            console.log(`${file} has been moved or deleted.`);
-          } else {
-            todos.push(todo);
-            if (index === files.length - 1) {
-              callback(null, todos);
+  return readdir(exports.dataDir)
+    .then( (files) => {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const fp = path.join(exports.dataDir, file);
+        files[i] = new Promise( (resolve, reject) => {
+          fs.readFile(fp, (err, text) => {
+            if (err) {
+              reject(err);
+            } else {
+              id = file.split('.')[0];
+              resolve({ id, text: text.toString()});
             }
-          }
+          });
         });
-      });
-    } else {
-      callback(null, []);
-    }
-  });
+      }
+      Promise.all(files)
+        .then((files) => {
+          callback(null, files);
+        });
+    })
+    .catch((err) => callback(err));
 };
+// => {
+//   return
+//     files.forEach((file) =>{
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(file);
+//       }
+//     })
+//   );
+// })
+// exports.readAll = (callback) => {
+//   let todos = [];
+//   let todo;
+//   fs.readdir(exports.dataDir, (err, files) => {
+//     if (err) {
+//       callback(new Error(`Error accessing ${datDir}`));
+//     } else if (files.length) {
+//       files.forEach( (file, index) => {
+//         id = file.split('.')[0];
+//         exports.readOne(id, (err, todo) => {
+//           if (err) {
+//             console.log(`${file} has been moved or deleted.`);
+//           } else {
+//             todos.push(todo);
+//             if (index === files.length - 1) {
+//               callback(null, todos);
+//             }
+//           }
+//         });
+//       });
+//     } else {
+//       callback(null, []);
+//     }
+//   });
+// };
 
 exports.readOne = (id, callback) => {
   file = path.join(exports.dataDir, `${id}.txt`);
